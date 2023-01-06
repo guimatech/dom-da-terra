e45b045239c1ba18739ba86ce98e1337c47f03f2
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2023-01-06 10:40:40 -0300

    chore: initial files

---
+++ src/main/java/io/github/guimatech/domdaterra/DomDaTerraApplication.java
@@ -0,0 +1,13 @@
+package io.github.guimatech.domdaterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomDaTerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomDaTerraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/guimatech/domdaterra/DomDaTerraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guimatech.domdaterra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomDaTerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
