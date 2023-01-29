0667425248391ee2eb2ce006dd5ab5d5603678e8
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2023-01-29 16:09:45 -0300

    feat: tela de perfil de usuário iniciada

---
+++ src/main/java/io/github/guimatech/domdaterra/infra/in/web/UserController.java
@@ -0,0 +1,16 @@
+package io.github.guimatech.domdaterra.infra.in.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        return "profile";
+    }
+}
